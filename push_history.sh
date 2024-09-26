ea797ecadb05e04a7f65205d2e5271989c50dba5
ajinkyadawange31045 <ajinkyadawange31045ard@gmail.com>
2024-09-26 07:54:01 +0000

    Commit at Thu Sep 26 07:54:01 AM UTC 2024

--- push_history.sh
+++ push_history.sh
@@ -1,18 +1,36 @@
 #!/bin/bash
 
 # Change directory to the repo
-cd ~/github_assignment/commit-history-task-bizz-training-period
+#cd ~/github_assignment/commit-history-task-bizz-training-period
 
 # Capture the history
 #history > history.txt
-history >  ~/github_assignment/commit-history-task-bizz-training-period/history.txt
+#history >  ~/github_assignment/commit-history-task-bizz-training-period/history.txt
 
 # Add changes to git
-git add .
+#git add .
 
 # Commit with the current time as the message
-git commit -m "$(date '+%Y-%m-%d %H:%M:%S')"
+#git commit -m "$(date '+%Y-%m-%d %H:%M:%S')"
 
 # Push to GitHub
-git push origin main
+#git push origin main
+
+#!/bin/bash
+
+# Change directory to the repo
+cd /home/ec2-user/github_assignment/commit-history-task-bizz-training-period
+
+# Capture the history from .bash_history to avoid session issues
+cat /home/ec2-user/.bash_history > /home/ec2-user/github_assignment/commit-history-task-bizz-training-period/history.txt
+
+# Ensure only the necessary file is added
+/usr/bin/git add history.txt
+
+# Commit with the current time as the message
+/usr/bin/git commit -m "$(date '+%Y-%m-%d %H:%M:%S')" 
+
+# Push to GitHub
+/usr/bin/git push origin main
+
 
