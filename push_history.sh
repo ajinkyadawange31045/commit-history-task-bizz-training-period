0f4dd8c38d2de571ca0b3ce8d6a52f37955d1b07
ajinkyadawange31045 <ajinkyadawange31045ard@gmail.com>
2024-09-26 07:45:57 +0000

    2024-09-26 07:45:57

--- push_history.sh
+++ push_history.sh
@@ -4,10 +4,11 @@
 cd ~/github_assignment/commit-history-task-bizz-training-period
 
 # Capture the history
-history > history.txt
+#history > history.txt
+history >  ~/github_assignment/commit-history-task-bizz-training-period/history.txt
 
 # Add changes to git
-git add history.txt
+git add .
 
 # Commit with the current time as the message
 git commit -m "$(date '+%Y-%m-%d %H:%M:%S')"
